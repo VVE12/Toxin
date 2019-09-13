6b7abf93b3c74da36904c43a79b824d3f01fe4b0
_VVE_ <vasilievvladislav12@gmail.com>
2019-09-13 14:13:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import "./scss/main.scss";
 import "./js/script.js";
 import "./js/datepicker.js";
 import "./js/jquery3.1.1.min.js"
-import css from 'air-datepicker/dist/css/datepicker.css';
-import datepicker from 'air-datepicker/dist/js/datepicker.js';
+import 'air-datepicker'
+/*import css from 'air-datepicker/dist/css/datepicker.css';
+import datepicker from 'air-datepicker/dist/js/datepicker.js';*/
